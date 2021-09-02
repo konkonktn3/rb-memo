94acd7d0cfa0a36a9c4b07d8b11e7af1e14c357d
‘kuma’ <km923.hr811@gmail.com>
2021-09-02 20:52:33 +0900

    still

---
+++ main.rb
@@ -0,0 +1,36 @@
+
+require "csv" #sample.csvを読み込む
+
+ puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+
+ memo_type = gets.to_s
+
+ if memo_type == 1
+     p "拡張機を除いたファイルを入力してください" 
+     file_name = gets.chomp
+
+     p "メモしたい内容を入力してください"
+     p "完了したらCtrl ＋ Dを押します"
+    
+     input = STDIN.read
+
+     CSV.open('#{file_name}.csv', 'w') do |csv|
+         csv << ["#{input}"]
+     end
+ else memo_type = 2
+    p "編集したいファイルを入力してください(拡張機を除き)" 
+     file_name = gets.chomp
+
+     p "編集したい内容を入力してください"
+     p "完了したらCtrl ＋ Dを押します"
+     input = gets.chomp
+     input = STDIN.read
+
+     CSV.open('#{file_name}.csv', 'a') do |csv|
+         csv << ["#{input}"]
+     end
+ end
+
+
+
+
