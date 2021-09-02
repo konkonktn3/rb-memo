1ae0af644c3c38ca6e1bd9d5f8ca78bdc0740498
‘kuma’ <km923.hr811@gmail.com>
2021-09-02 23:40:48 +0900

    "

--- main.rb
+++ main.rb
@@ -5,31 +5,32 @@ require "csv" #sample.csvを読み込む
 
  memo_type = gets.to_s
 
- if memo_type == 1
-     p "拡張機を除いたファイルを入力してください" 
-     file_name = gets.chomp
+if memo_type = "1"
+    p "拡張機を除いたファイルを入力してください" 
+    file_name = gets.chomp
 
-     p "メモしたい内容を入力してください"
-     p "完了したらCtrl ＋ Dを押します"
-    
-     input = STDIN.read
+    p "メモしたい内容を入力してください"
+    p "完了したらCtrl＋Dを押します"
+    input = STDIN.read 
 
-     CSV.open('#{file_name}.csv', 'w') do |csv|
-         csv << ["#{input}"]
-     end
- else memo_type = 2
+        CSV.open("#{file_name}.csv", "w") do |csv|
+         csv.puts ["#{input}"]
+        end
+
+elsif memo_type = "2"
     p "編集したいファイルを入力してください(拡張機を除き)" 
-     file_name = gets.chomp
+    file_name = gets.chomp
 
-     p "編集したい内容を入力してください"
-     p "完了したらCtrl ＋ Dを押します"
-     input = gets.chomp
-     input = STDIN.read
+    p "編集したい内容を入力してください"
+    p "完了したらCtrl＋Dを押します"
+    input = STDIN.read
 
-     CSV.open('#{file_name}.csv', 'a') do |csv|
-         csv << ["#{input}"]
-     end
- end
+        CSV.open("#{file_name}.csv", "a") do |csv|
+         csv.puts ["#{input}"]
+        end
+       
+end
+
 
 
 
