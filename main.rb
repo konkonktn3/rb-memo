57c9af7087048716c73b542b3a34c47023f6d8cd
‘kuma’ <km923.hr811@gmail.com>
2021-09-03 03:07:47 +0900

    csv

--- main.rb
+++ main.rb
@@ -1,37 +1,31 @@
 
 require "csv" #sample.csvを読み込む
 
- puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 
- memo_type = gets.to_s
+memo_type = gets.to_s
 
-if memo_type = "1"
-    p "拡張機を除いたファイルを入力してください" 
-    file_name = gets.chomp
+    if memo_type = "1"
+        p "拡張機を除いたファイルを入力してください" 
+        file_name = gets.chomp
 
-    p "メモしたい内容を入力してください"
-    p "完了したらCtrl＋Dを押します"
-    input = STDIN.read 
+        p "メモしたい内容を入力してください"
+        p "完了したらCtrl＋Dを押します"
+        input = STDIN.read 
 
-        CSV.open("#{file_name}.csv", "w") do |csv|
-         csv.puts ["#{input}"]
-        end
+        csv = CSV.open("#{file_name}.csv","w")
+            csv.puts ["#{input}"]
+        csv.close
 
-elsif memo_type = "2"
-    p "編集したいファイルを入力してください(拡張機を除き)" 
-    file_name = gets.chomp
+    elsif memo_type = "2"
+        p "編集したいファイル名を入力してください" 
+        file_name = gets.chomp
 
-    p "編集したい内容を入力してください"
-    p "完了したらCtrl＋Dを押します"
-    input = STDIN.read
+        p "編集したい内容を入力してください"
+        p "完了したらCtrl＋Dを押します"
+        input = STDIN.read
 
-        CSV.open("#{file_name}.csv", "a") do |csv|
-         csv.puts ["#{input}"]
-        end
-       
-end
-
-
-
-
-
+        csv = CSV.open("#{file_name}.csv","a") 
+            csv.puts ["#{input}"]
+        csv.close   
+    end
